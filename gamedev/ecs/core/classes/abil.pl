5aa793e425529e24ec61a380a6fc3a5856443bfb
Siyuan Chen <chansey97@outlook.com>
2022-06-09 03:34:10 +0800

    Add missing classes.

--- gamedev/ecs/core/classes/abil.pl
+++ gamedev/ecs/core/classes/abil.pl
@@ -1,3 +1,5 @@
+%% TODO:
+%% Use Prolog record s_field?
 
 %% TODO:
 %% Better constraints? e.g. min_value, max_value?
@@ -6,6 +8,33 @@ class(id = c_abil, super = null,
       [field(name = name,
              pred = string,
              default = "")
+      ]).
+
+class(id = c_abil_move, super = c_abil,
+      [field(name = cost_cooldown,
+             pred = number,
+             default = 0)
+      ]).
+
+class(id = c_abil_attack, super = c_abil,
+      [field(name = cost_cooldown,
+             pred = number,
+             default = 0)
+      ]).
+
+class(id = c_abil_keyboard_move, super = c_abil,
+      [field(name = left_key,
+             pred = number,
+             default = 37),
+       field(name = up_key,
+             pred = number,
+             default = 38),
+       field(name = right_key,
+             pred = number,
+             default = 39),
+       field(name = down_key,
+             pred = number,
+             default = 40)
       ]).
 
 class(id = c_abil_effect, super = c_abil,
@@ -30,3 +59,12 @@ class(id = c_abil_effect_target, super = c_abil_effect,
       [field(name = range,
              pred = number,
              default = 0)]).
+
+class(id = c_abil_morph, super = c_abil,
+      [field(name = cost_energy,
+             pred = number,
+             default = 0),
+       field(name = template,
+             pred = atom,
+             default = null)
+       ]).
