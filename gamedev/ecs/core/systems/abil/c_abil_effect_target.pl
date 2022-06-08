fb040d9394ef341320a335c25c5d46d40ad4189d
Siyuan Chen <chansey97@outlook.com>
2022-06-08 23:38:55 +0800

    update comments

--- gamedev/ecs/core/systems/abil/c_abil_effect_target.pl
+++ gamedev/ecs/core/systems/abil/c_abil_effect_target.pl
@@ -31,8 +31,8 @@ c(AID, abil_target, target()) # passive
 %% abil 的 “main effect” 基于不同的 c_effect 可能不同，比如：c_effect_launch_missile 就是基于 impact_location 来决定的。
 %% abil 自己也有一个 target filter，但并不重要
 
-%% 但此时的effect还没创建，所以无法 check
-%% 因此，我们要把　template　也作为　entitty　处理 which can handle event
+%% 但此时的 effect 还没创建，所以无法 check
+%% 因此，我们要把 template 也作为 entitty 处理 which can handle event
 
 %% So abil 系统 并不完全仅仅基于 AbilID，而应该基于 AbilID 和 AbilTempalteID 的关联
 c_abil_effect_target:event_abil_check @
