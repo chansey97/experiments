5d3e9da7b6254f2bc318ed90051d4fb6eeda13de
Siyuan Chen <chansey97@outlook.com>
2022-06-09 03:48:52 +0800

    Add duplicate templates check.

--- gamedev/ecs/core/utils/raw_template.pl
+++ gamedev/ecs/core/utils/raw_template.pl
@@ -20,11 +20,14 @@ load_raw_template(File) :-
   %% format("load_raw_template ~w ~n", [File]),
   read_file_to_terms(File, Terms, []),
   maplist([Term]>>
-         (   Term =.. [template, catalog=Catalog, id=ID, Heads, Fields],
-             %% format("load_raw_template ~w ~w ~n", [Catalog, ID]),  
-             %% TODO: check duplicate templates!             
-             maplist({Catalog, ID}/[Head]>>(Head=(K=V), assertz(raw_template(Catalog, ID, K, V))), Heads),
-             maplist({Catalog, ID}/[Field]>>(Field=(K=V), assertz(raw_template_field(Catalog, ID, K, V))), Fields)
+         (   Term =.. [template, catalog=Catalog, id=ID, Heads, Fields]
+         ->  (   raw_template(Catalog, ID, _, _)
+             ->  format("load_raw_template Error. The template ~w ~w is duplicate.~n", [Catalog, ID]),
+                 false
+             ;   maplist({Catalog, ID}/[Head]>>(Head=(K=V), assertz(raw_template(Catalog, ID, K, V))), Heads),
+                 maplist({Catalog, ID}/[Field]>>(Field=(K=V), assertz(raw_template_field(Catalog, ID, K, V))), Fields)
+             )
+         ;   format("load_raw_template Warning. The term ~w is not a template, ignored.~n", [Term])
          ), Terms).
 
 raw_template_class_get(Catalog, ID, Class) :-
