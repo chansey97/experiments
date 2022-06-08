43b016a4835310303646407e764e1de75f6e8556
Siyuan Chen <chansey97@outlook.com>
2022-06-09 04:55:46 +0800

    The templates in core now are useless.

--- gamedev/ecs/core/templates/actor.pl
+++
@@ -1,8 +0,0 @@
-
-%% template(catalog = actor, id = t_actor_model,
-%%          [class = c_actor_model
-%%          ],     
-%%          [name = "Bear",
-%%           %% texture = "empty.png"     
-%%          ]).
-
