5c1d810ad49cd8c8d16600d73ce58a2154297a6a
Siyuan Chen <chansey97@outlook.com>
2022-06-08 10:23:34 +0800

    Add some comments.

--- gamedev/ecs/core/classes/abil.pl
+++ gamedev/ecs/core/classes/abil.pl
@@ -1,4 +1,31 @@
 
+%% TODO:
+%% The class and class_field here are just machine representation and not very readable.
+%% May be better to have configration representation which is more readable.
+
+%% FIXME:
+%% Although it is a machine currently, it is not a good representation...
+%% It seems that there is no need to have machine representation, because performance doesn't matter here.
+%% When running, we mostly use template entities.
+
+%% TODO:
+%% Better constraints? e.g. min_value, max_value?
+
+%% class(name = c_abil_effect, super = c_abil
+%%      [field(name = transient,
+%%             pred = atom,
+%%             default = false),
+%%       field(name = cost_cooldown,
+%%             pred = number,
+%%             default = 0),
+%%       field(name = cost_energy,
+%%             pred = number,
+%%             default = 0),
+%%       field(name = effects,
+%%             pred = list,
+%%             default = [])
+%%      ]).
+
 %% c_abil
 class(c_abil, super, null).
 class_field(c_abil, name, {pred:string, default:""}).
@@ -16,3 +43,4 @@ class(c_abil_effect_instant, super, c_abil_effect).
 %% c_abil_effect_target
 class(c_abil_effect_target, super, c_abil_effect).
 class_field(c_abil_effect_target, range, {pred:integer, default:0}).
+
