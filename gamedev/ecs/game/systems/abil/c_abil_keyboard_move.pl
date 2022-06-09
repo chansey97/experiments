a2fca294acf20598fe252376509e339e29c43e8e
Siyuan Chen <chansey97@outlook.com>
2022-06-09 08:21:42 +0800

    Fix a typo.

--- gamedev/ecs/game/systems/abil/c_abil_keyboard_move.pl
+++ gamedev/ecs/game/systems/abil/c_abil_keyboard_move.pl
@@ -5,7 +5,7 @@ c(EID, event_abil_create, c_abil_keyboard_move)
 <=>
   true.
 
-c_abil_keyboard_move_create @
+c_abil_keyboard_move_destroy @
 c(EID, template, Template) # passive
 \
 c(EID, event_abil_destroy, c_abil_keyboard_move)
