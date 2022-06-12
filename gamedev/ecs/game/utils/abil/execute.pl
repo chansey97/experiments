afc4343c07d049017dbdfa49a5fb5c5d63018b4c
Siyuan Chen <chansey97@outlook.com>
2022-06-13 03:58:58 +0800

    update code

--- gamedev/ecs/game/utils/abil/execute.pl
+++ gamedev/ecs/game/utils/abil/execute.pl
@@ -18,64 +18,50 @@ abil_on_execute(c_abil, A_EID)
 
 abil_on_execute(c_abil, A_EID) <=> true.
 
+%% c_abil_effect
+c(A_EID, type, abil) #passive,
+c(A_EID, template, Template) # passive,
+c(A_EID, owner_id, U_EID) # passive,
+c(A_EID, cooldown, Cooldown) # passive,
+c(T_EID, type, template) #passive,
+c(T_EID, catalog, abil) #passive,
+c(T_EID, id, Template) #passive,
+c(T_EID, transient, Transient) #passive,
+c(T_EID, cost_energy, CostEnergy) #passive,
+c(T_EID, cost_cooldown, CostCooldown) #passive,
+c(U_EID, type, unit) #passive,
+c(U_EID, energy, Energy) #passive
+\
+abil_on_execute(c_abil_effect, A_EID) 
+<=>
+  (   \+ Transient,
+      get_component(U_EID, executing_abil, ExecutingAbil)
+  ->  abil_cancel(ExecutingAbil),
+      c(U_EID, executing_abil, A_EID)
+  ;   true
+  ), 
+  Energy2 is Energy - CostEnergy,
+  set_component(U_EID, energy, Energy2),
+  set_component(A_EID, cooldown, CostCooldown),
+  create_effect(Template, U_EID, Target)  
+  .
 
-%% c_abil_effect_execute @
-%% c(A_EID, type, abil) #passive,
-%% c(A_EID, template, Template) # passive,
-%% c(A_EID, owner_id, U_EID) # passive,
-%% c(A_EID, cooldown, Cooldown) # passive,
-%% c(U_EID, type, unit) #passive,
-%% c(U_EID, energy, Energy) #passive
-%% \
-%% c(A_EID, event_abil_execute, c_abil_effect)
-%% <=>
-%%   (   \+ Transient,
-%%       get_component(U_EID, executing_abil, ExecutingAbil)
-%%   ->  abil_cancel(ExecutingAbil),
-%%       c(U_EID, executing_abil, A_EID)
-%%   ;   true
-%%   ),
-%%   template_field_value_get(abil, Template, transient, Transient),
-%%   template_field_value_get(abil, Template, cost_energy, CostEnergy),
-%%   template_field_value_get(abil, Template, cost_cooldown, CostCooldown),  
-%%   Energy2 is Energy - CostEnergy,
-%%   set_component(U_EID, energy, Energy2),
-%%   set_component(A_EID, cooldown, CostCooldown).
+abil_on_execute(c_abil_effect, A_EID)  <=> true.
 
-%% c_abil_effect_execute_cleanup @
-%% c(A_EID, event_abil_execute, c_abil_effect) <=> true.
+%% c_abil_effect_instant
+abil_on_execute(c_abil_effect_instant, A_EID)
+<=>
+  abil_on_execute(c_abil_effect, A_EID).
 
+abil_on_execute(c_abil_effect_instant, A_EID) <=> true.
 
-%% c_abil_effect_instant:event_abil_execute @
-%% c(A_EID, template, Template) # passive,
-%% c(A_EID, owner_id, U_EID) # passive
-%% \  
-%% c(A_EID, event_abil_execute, c_abil_effect_instant)
-%% <=>
-%%   sub_class(c_abil_effect_instant, SuperClass),
-%%   c(A_EID, event_abil_execute, SuperClass),
-%%   template_field_value_get(abil, Template, effects, Effs),    
-%%   maplist({U_EID}/[Eff, EffID]>>create_effect(Eff, U_EID, U_EID, EffID), Effs, EffIDs).
+%% c_abil_effect_target
+abil_on_execute(c_abil_effect_target, A_EID)
+<=>
+  abil_on_execute(c_abil_effect, A_EID).
 
-%% c(A_EID, event_abil_execute, c_abil_effect_instant)
-%% <=>
-%%   true.
+abil_on_execute(c_abil_effect_target, A_EID) <=> true.
 
-
-%% c_abil_effect_target:event_abil_execute @
-%% c(A_EID, template, Template) # passive,
-%% c(A_EID, owner_id, U_EID) # passive
-%% \  
-%% c(A_EID, event_abil_execute, c_abil_effect_target)
-%% <=>
-%%   sub_class(c_abil_effect_target, SuperClass),
-%%   c(A_EID, event_abil_execute, SuperClass),
-%%   template_field_value_get(abil, Template, effects, Effs),    
-%%   maplist({U_EID}/[Eff, EffID]>>create_effect(Eff, U_EID, U_EID, EffID), Effs, EffIDs).
-
-%% c(A_EID, event_abil_execute, c_abil_effect_target)
-%% <=>
-%%   true.
 
 %% c_abil_morph_execute @
 %% c(A_EID, template, AbilTemplate) # passive,
